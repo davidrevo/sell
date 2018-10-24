e5b9aa9beadb34a343a718b4f3fae1533c133eac
David Dai <david.dai@mcon-group.com>
2018-10-24 16:23:34 +0800

    food detail title price cart control

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="cartcontrol">
     <div class="cart-decrease icon-remove_circle_outline" v-show="food.count > 0"
-      @click="decreaseCart" transition="move">
+      @click.stop.prevent="decreaseCart" transition="move">
       <span class="inner icon-remove_circle_outline"></span>
     </div>
     <div class="cart-count" v-show="food.count > 0">
       {{food.count}}
     </div>
-    <div class="cart-add icon-add_circle" @click="addCart"></div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
   </div>
 </template>
 
